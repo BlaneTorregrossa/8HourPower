ada0d347e0cb2f4e9a78832371369a2a565834d8
BlaneTorregrossa <blanet97@cox.net>
2017-05-05 17:39:03 -0500

    2 hours of worthwile work in the span of 8 hours

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject Player;
+    public Rigidbody Player_RB;
+    public Text FuelText;
+
+    protected Vector3 PlayerGroundedPosition;
+    protected Vector3 PlayerLastPosition;
+    protected int Score;
+
+    private float Fuel;
+    private bool onFloor;
+    private bool Falling;
+    private float MovementUnit = 1f;
+
+
+    protected void Movemnt()
+    {
+        //Moves Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Player.transform.position.x >= 61)
+            {
+                Player.transform.position = PlayerLastPosition;
+            }
+            else
+                Player.transform.position += new Vector3(MovementUnit, 0, 0);
+        }
+
+        //Moves Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Player.transform.position.x <= -61)
+            {
+                Player.transform.position = PlayerLastPosition;
+            }
+            else
+                Player.transform.position += new Vector3(-MovementUnit, 0, 0);
+
+        }
+
+        //Jumps
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (Player.transform.position.y >= 24.4f && Fuel > 0)
+            {
+                Player.transform.position = PlayerLastPosition;
+            }
+            else if (Fuel > 0)
+            {
+                Player.transform.position += new Vector3(0, MovementUnit * 2, 0);
+                onFloor = false;
+                if (onFloor == false && Player.transform.position.y > PlayerGroundedPosition.y * 3)
+                {
+                    Falling = true;
+                    Player.transform.position += new Vector3(0, -MovementUnit, 0);
+                }
+            }
+        }
+
+        //Falls
+        if (!Input.GetKey(KeyCode.Space))
+        {
+            if (Player.transform.position.y <= -23.9f)
+            {
+                Player.transform.position = PlayerLastPosition;
+                onFloor = true;
+            }
+            else
+                Player.transform.position += new Vector3(0, -MovementUnit * 1, 0);
+            Falling = true;
+        }
+
+    }
+
+    // Sets Defaults
+    void Start()
+    {
+        onFloor = false;
+        Falling = true;
+        Fuel = 100.0f;
+    }
+
+    void Update()
+    {
+        PlayerLastPosition = Player.transform.position;
+        Movemnt();
+        if (onFloor == true)
+        {
+            PlayerGroundedPosition = Player.transform.position;
+            Falling = false;
+        }
+        if (onFloor == false)
+        {
+            Fuel -= 0.1f;
+            FuelText.text = "Fuel Is: " + Fuel.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleGrid.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGrid : PlayerMovement
+{
+
+    //public Vector2 InvisGraphVec = new Vector2(0, 0);
+    //public List<Vector2> InvisGraph;
+    public GameObject Objectives;
+    public Rigidbody Objective_RB;
+
+    protected int RandNum = 0;
+    protected int ObjectivesShown = 0;
+
+    private Vector3 TargetPos1 = new Vector3(-31, -12, -60);
+    private Vector3 TargetPos2 = new Vector3(-31, 12, -60);
+    private Vector3 TargetPos3 = new Vector3(31, -12, -60);
+    private Vector3 TargetPos4 = new Vector3(31, 12, -60);
+    //private int XCount = 0;
+
+
+    //public void GenGraph()
+    //{
+    //    if (InvisGraphVec.x < 1 || InvisGraphVec.y < 1)
+    //    {
+    //        if (InvisGraphVec.x != XCount)
+    //        {
+    //            if (InvisGraph.Contains(TargetPos1))
+    //            {
+    //                InvisGraph.Add(TargetPos3);
+    //            }
+    //            else
+    //                InvisGraph.Add(TargetPos1);
+    //            InvisGraphVec.x++;
+    //        }
+    //        if (InvisGraphVec.x == XCount)
+    //        {
+    //            if (InvisGraph.Contains(TargetPos2))
+    //            {
+    //                InvisGraph.Add(TargetPos4);
+    //            }
+    //            else
+    //                InvisGraph.Add(TargetPos2);
+    //            InvisGraphVec.y++;
+    //            Objectives.transform.position = TargetPos1;
+    //            Instantiate(Objectives);
+    //        }
+    //    }
+    //}
+
+
+    public void PlaceObjective()
+    {
+        if (RandNum == 0)
+           RandNum = Random.Range(1, 4);
+        if (RandNum == 1 && ObjectivesShown == 0)
+        {
+            Objectives.transform.position = TargetPos1;
+            Instantiate(Objectives);
+            ObjectivesShown++;
+            Objectives.SetActive(true);
+        }
+        if (RandNum == 2 && ObjectivesShown == 0)
+        {
+            Objectives.transform.position = TargetPos2;
+            Instantiate(Objectives);
+            ObjectivesShown++;
+            Objectives.SetActive(true);
+        }
+        if (RandNum == 3 && ObjectivesShown == 0)
+        {
+            Objectives.transform.position = TargetPos3;
+            Instantiate(Objectives);
+            ObjectivesShown++;
+            Objectives.SetActive(true);
+        }
+        if (RandNum == 4 && ObjectivesShown == 0)
+        {
+            Objectives.transform.position = TargetPos4;
+            Instantiate(Objectives);
+            ObjectivesShown++;
+            Objectives.SetActive(true);
+        }
+    }
+
+
+ 
+	
+	void Update ()
+    {
+        PlaceObjective();
+	}
+}
